ac087740f32c11573db9ff0a14cb4a1e10e04157
Sebastian More <sebitas.mo.si@gmail.com>
2024-09-19 21:08:47 -0500

    Me quede en desempaquetar listas

---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,21 @@
+numeros = [1,2,3]
+print(numeros)
+
+
+letras = ["a","b","c"]
+palabras = ["chanchito","feliz"]
+palabrasFelices = ["Chanchito", "feliz", "Felipe", "alumno"]
+
+print(palabrasFelices)
+
+booleans=[True,False,True,True]
+
+matriz = [[0,1],[1,0]]
+print(matriz)
+
+ceros = [0]*10
+
+alfanumerico = numeros + letras
+rango = list(range(1,11))
+
+print(rango)
---
+++ tipos-avanzados/02-manipulando-lista.py
@@ -0,0 +1,16 @@
+
+
+mascotas = ["Wolfgang","Pelusa","Pulga","Copito"]
+print(mascotas[0])
+mascotas[0]="Bicho"
+print(mascotas)
+
+print(mascotas[:3])
+print(mascotas[2:])
+print(mascotas[-1])
+print(mascotas[::2])
+
+numeros=list(range(1,21))
+print(numeros)
+print(numeros[::2])
+print(numeros[1::2])
